2a5f87008b040313341111109ff6062af787597a
Felipe Orlando <fobsouza@gmail.com>
2020-09-21 20:55:47 -0300

    create reader entity

--- lib/pricefy.rb
+++ lib/pricefy.rb
@@ -1,3 +1,12 @@
 class Pricefy
-  #
+  def initialize(filepath:, destination_path:, reader:)
+    @filepath = filepath
+    @destination_path = destination_path
+    @reader = reader
+  end
+
+  def run
+    items = @reader.get(@filepath)
+    p items
+  end
 end
