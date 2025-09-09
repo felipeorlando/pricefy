e1a6c6c32b9ccaeb9cb5653fda760ce919225dc9
Felipe Orlando <fobsouza@gmail.com>
2025-09-09 00:14:00 -0300

    first commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in pricefy.gemspec
+gemspec
+
+gem "rake", "~> 13.2"
+gem "rspec", "~> 3.13"
---
+++ spec/pricefy_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Pricefy do
+  it "has a version number" do
+    expect(Pricefy::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
